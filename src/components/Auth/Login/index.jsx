4622cae74e523c7ed5389d1c638f4d66059d8c1d
nirglus <nirgluskin@gmail.com>
2024-02-04 12:19:58 +0200

    Add login and signup

--- src/components/Auth/Login/index.jsx
+++ src/components/Auth/Login/index.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+function Login(props){
 
-function Login() {
   return (
-    <div>
-      
-    </div>
+      <div className="loginCont">
+          <h2>Resume<span className="matrix">Matrix</span></h2>
+          <p>Login</p>
+          <form className="authForm" onSubmit={props.submitHandler}>
+              <input
+                  onChange={props.changeHandler}
+                  type="email"
+                  name="email"
+                  placeholder="example123@email.com"
+                  required
+              />
+              <input
+                  onChange={props.changeHandler}
+                  type="password"
+                  name="password"
+                  placeholder="Enter your password"
+                  required
+              />
+              <button type="submit">Sign In</button>
+          </form>
+      </div>
   )
 }
 
-export default Login
+export default Login;
--- src/components/Auth/SignUp/index.jsx
+++ src/components/Auth/SignUp/index.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-
-function SignUp() {
+function SignUp(props){
   return (
-    <div>
-      
-    </div>
+      <div className="loginCont">
+          <h2>Resume<span className="matrix">Matrix</span></h2>
+          <p>Register</p>
+          <form className="authForm" onSubmit={props.submitHandler}>
+              <input
+                  onChange={props.changeHandler}
+                  type="text"
+                  name="nickname"
+                  placeholder="Enter a username"
+                  required
+              />
+              <input
+                  onChange={props.changeHandler}
+                  type="email"
+                  name="email"
+                  placeholder="example123@email.com"
+                  required
+              />
+              <input
+                  onChange={props.changeHandler}
+                  type="password"
+                  name="password"
+                  placeholder="Enter your password"
+                  required
+              />
+              <button type="submit">Create Account</button>
+          </form>
+      </div>
   )
 }
 
-export default SignUp
+export default SignUp;
