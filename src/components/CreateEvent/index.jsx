962a47f930f2ec7486d5292d7ddf40516963c50b
yizak223 <139069183+yizak223@users.noreply.github.com>
2024-02-04 12:37:07 +0200

    options

--- src/components/CreateEvent/index.jsx
+++ src/components/CreateEvent/index.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 function CreateEvent() {
   return (
     <div>
-      
+      <h5>Picnic</h5>
+      <h5>Sea</h5>
+      <h5>Trip</h5>
+      <h5>Camping</h5>
+      <h5>Flight</h5>
+      <h5>Cultural Exploration</h5>
+      <h5>Other</h5>
     </div>
   )
 }
