165880e412361a045e87ebac3cccf4541cbf2e97
yizak223 <139069183+yizak223@users.noreply.github.com>
2024-02-04 21:47:47 +0200

    user event dont show if no log in

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,11 @@ import Join from "./components/Join";
 import UserEvents from "./pages/UserEvents/Index";
 import "./App.css";
 import Home from "./pages/Home";
+import { useContext } from "react";
+import { UserContext } from "./components/context/User";
 
 function App() {
+  const {user}=useContext(UserContext)
   return (
     <>
       <BrowserRouter>
@@ -18,7 +21,10 @@ function App() {
           <Route path="/Auth" element={<Auth />} />
           <Route path="/createEvent" element={<CreateEvent />} />
           <Route path="/event:id" element={<Join />} />
+          {user?
           <Route path="/UserEvents" element={<UserEvents />} />
+          :null
+          }
         </Routes>
       </BrowserRouter>
 
